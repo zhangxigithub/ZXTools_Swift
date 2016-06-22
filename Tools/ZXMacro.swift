b6e2cf53ad308224431ef9636e8c944b502881dc
zhangxi <zhangxi_1989@sina.com>
2016-06-22 17:13:34 +0800

    0.1.4

--- Tools/ZXMacro.swift
+++ Tools/ZXMacro.swift
@@ -1,10 +1,13 @@
 import UIKit
 
+public struct ZXMacroStruct{
 
+    let DeviceId      = UIDevice.currentDevice().identifierForVendor!.UUIDString
+    
+    let ScreenWidth   = UIScreen.mainScreen().bounds.size.width
+    let ScreenHeight  = UIScreen.mainScreen().bounds.size.height
+    
+    let SystemVersion = (UIDevice.currentDevice().systemVersion as NSString).floatValue
+}
+let ZXMacro = ZXMacroStruct()
 
-let DeviceId      = UIDevice.currentDevice().identifierForVendor!.UUIDString
-
-let ScreenWidth   = UIScreen.mainScreen().bounds.size.width
-let ScreenHeight  = UIScreen.mainScreen().bounds.size.height
-
-let SystemVersion = (UIDevice.currentDevice().systemVersion as NSString).floatValue
