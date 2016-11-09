6bbe6dd2b090de67f782b104f7991c0dbbf56aa7
zhangxi <zhangxi_1989@sina.com>
2016-11-09 15:05:29 +0800

    up

--- Tools/String+ZXTools.swift
+++ Tools/String+ZXTools.swift
@@ -5,19 +5,39 @@
 //  Created by zhangxi on 15/7/31.
 //  Copyright (c) 2015年 zhangxi. All rights reserved.
 //
-
 import Foundation
 
 public extension String
 {
     public func stringRemovedLastChar()->String
     {
-        let last1 = self.characters.index(self.endIndex, offsetBy: -1)
-        return self.substring(to: last1)
+        let last1 = self.endIndex.advancedBy(-1)
+        return self.substringToIndex(last1)
     }
     
-    public func encodeURL() -> URL
+    public func encodeURL() -> NSURL
     {
-        return URL(string: self.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!)!;
+        if let originURL = NSURL(string: self)
+        {
+            return originURL
+        }else
+        {
+            if let url = NSURL(string: self.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!)
+            {
+                // 把URL最后一个#解码，还不确定会不会有问题 张玺
+                if  let range = url.absoluteString?.rangeOfString("%23", options: NSStringCompareOptions.BackwardsSearch)
+                {
+                    let newURL = url.absoluteString?.stringByReplacingCharactersInRange(range, withString: "#")
+                    return NSURL(string: newURL!)!
+                }else
+                {
+                    return url
+                }
+                
+            }else
+            {
+                return NSURL(string: "")!
+            }
+        }
     }
 }
